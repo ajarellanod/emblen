e5739afc86d30ed3fab46e9e4bd1d887ca5b28fe
Nelmer Ayala <nelmerayala@gmail.com>
2020-12-17 21:00:53 -0400

    modificacion de models y admin

--- emblen/apps/formulacion/admin.py
+++ emblen/apps/formulacion/admin.py
@@ -9,8 +9,56 @@ admin.site.register(models.Dependencia)
 
 admin.site.register(models.Departamento)
 
-admin.site.register(models.Proyecto)
+admin.site.register(models.Estado)
+
+admin.site.register(models.Municipio)
+
+admin.site.register(models.Parroquia)
+
+admin.site.register(models.FuenteFinanciamiento)
+
+admin.site.register(models.CentroCosto)
+
+admin.site.register(models.UnidadEjecutora)
+
+admin.site.register(models.SectorDesarrollador)
+
+admin.site.register(models.UnidadMedida)
+
+admin.site.register(models.TipoBeneficiario)
+
+admin.site.register(models.PeriodoActualizacion)
+
+admin.site.register(models.CondicionPrograma)
+
+admin.site.register(models.Programa)
+
+admin.site.register(models.LineaPlan)
+
+admin.site.register(models.LineaPrograma)
+
+admin.site.register(models.PlanDesarrollo)
+
+admin.site.register(models.AreaInversion)
+
+admin.site.register(models.CategoriaAreaInversion)
+
+admin.site.register(models.EstatusFinanciamientoExterno)
+
+admin.site.register(models.TipoAreaInversion)
 
 admin.site.register(models.AccionEspecifica)
 
 admin.site.register(models.Partida)
+
+admin.site.register(models.Estimacion)
+
+admin.site.register(models.TipoGasto)
+
+admin.site.register(models.TipoOrganismo)
+
+admin.site.register(models.AccionInterna)
+
+admin.site.register(models.CCostoAccInt)
+
+admin.site.register(models.CtasCCostoAInt)
